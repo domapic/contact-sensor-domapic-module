146b921da2ef61904a1a4587051ccfccd82acc76
Javier Brea <javier.brea@gmail.com>
2018-12-15 12:47:42 +0100

    Add contact sensor module code

--- server.js
+++ server.js
@@ -1,1 +1,51 @@
 'use strict'
+
+const path = require('path')
+
+const domapic = require('domapic-service')
+const gpioIn = require('gpio-in-domapic')
+
+const {
+  ABILITY_NAME,
+  DEBOUNCE_OPTION,
+  ABILITY_DESCRIPTION,
+  ABILITY_STATE_DESCRIPTION,
+  ABILITY_EVENT_DESCRIPTION
+} = require('./lib/statics')
+
+const options = require('./lib/options')
+
+domapic.createModule({
+  packagePath: path.resolve(__dirname),
+  customConfig: options
+}).then(async dmpcModule => {
+  const contactSensor = new gpioIn.Gpio(dmpcModule, {
+  }, {
+    debounceTimeout: DEBOUNCE_OPTION
+  })
+
+  await dmpcModule.register({
+    [ABILITY_NAME]: {
+      description: ABILITY_DESCRIPTION,
+      data: {
+        type: 'boolean'
+      },
+      state: {
+        description: ABILITY_STATE_DESCRIPTION,
+        handler: () => contactSensor.status
+      },
+      event: {
+        description: ABILITY_EVENT_DESCRIPTION
+      }
+    }
+  })
+
+  await contactSensor.init()
+
+  contactSensor.events.on(gpioIn.Gpio.eventNames.CHANGE, newValue => {
+    dmpcModule.tracer.debug(ABILITY_EVENT_DESCRIPTION, newValue)
+    dmpcModule.events.emit(ABILITY_NAME, newValue)
+  })
+
+  return dmpcModule.start()
+})
